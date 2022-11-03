c037e6a36ccb2615f5e60f4bc8f945c626565f15
LavanyaB1605 <lavanya.bhaskaran16@gmail.com>
2022-11-04 00:36:12 +0530

    Added Uc4

--- src/Uc1FirstName.java
+++ src/Uc1FirstName.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Uc1FirstName {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter first name:");
-        String FirstName = sc.next();
+        String FirstName = sc.nextLine();
         String RegEx = "[A-Z][a-z]{3,}";
         boolean check = FirstName.matches(RegEx);
         if(check) {
@@ -14,7 +16,7 @@ public class Uc1FirstName {
             System.out.println("Entered name is not valid");
         }
         System.out.println("Please enter last name:");
-        String LastName = sc.next();
+        String LastName = sc.nextLine();
         boolean check1 = LastName.matches(RegEx);
         if(check1) {
             System.out.println("Entered name is valid");
@@ -23,13 +25,21 @@ public class Uc1FirstName {
             System.out.println("Entered name is not valid");
         }
         System.out.println("Please enter Email Id:");
-        String Email = sc.next();
-        boolean check2 = Email.matches(RegEx);
-        if(check1) {
+        String Email = sc.nextLine();
+        String RegEx1 = "^[A-Za-z0-9+_.-]+@(.+)$";
+        boolean check2 = Email.matches(RegEx1);
+        if(check2) {
             System.out.println("Entered ID is valid");
         }
         else {
             System.out.println("Entered ID is not valid");
         }
+       System.out.println("Please enter Phone Number:");
+        String num = sc.nextLine();
+        //String RegEx2 = "[91]{2} [6-9]{1}[0-9]{9}";
+        String RegEx2 = "[91]{2} [6-9]{1}[0-9]{9}";
+        Pattern pattern = Pattern.compile(RegEx2);
+        Matcher matcher = pattern.matcher(num);
+        System.out.println(num +" : "+ matcher.matches());
     }
 }
